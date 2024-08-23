4d1d03318f6ac3968354273af65da5b840863e58
arroyv <arroyv@uw.edu>
2024-08-23 13:41:43 -0700

    updated light transmitter and CO2 Transmitter code

--- python/src/CO2/CO2.py
+++ python/src/CO2/CO2.py
@@ -1,5 +1,5 @@
 # '''
-#   Author: Stephany Ayala-Cerna
+#   Author: Stephany Ayala-Cerna, Vicente Arroyos
 # '''
 
 # import minimalmodbus
@@ -33,29 +33,27 @@
 # print(f"CO2 Concentration {data} ppm")
 
 import minimalmodbus
-import serial
+import time
 
-# Define the instrument (replace '/dev/ttyUSB0' with the appropriate port for your setup)
-instrument = minimalmodbus.Instrument('dev/ttyACM0', 1)  # Port name and slave address (default is 0x01)
-instrument.serial.baudrate = 4800                         # Baudrate (default is 4800)
-instrument.serial.bytesize = 8                            # Number of data bits to be requested
-instrument.serial.parity   = serial.PARITY_NONE           # Parity setting
-instrument.serial.stopbits = 1                            # Number of stop bits
-instrument.serial.timeout  = 1                            # Timeout in seconds
+# Configure the instrumentcd 
+instrument = minimalmodbus.Instrument('/dev/ttyACM0', 1)  # Replace with your serial port and address
+instrument.serial.baudrate = 4800  # Default baudrate from the document
+instrument.serial.bytesize = 8
+instrument.serial.parity = minimalmodbus.serial.PARITY_NONE
+instrument.serial.stopbits = 1
+instrument.serial.timeout = 1  # seconds
 
-instrument.mode = minimalmodbus.MODE_RTU                  # Modbus mode (RTU or ASCII)
-instrument.clear_buffers_before_each_transaction = True   # Clear buffers to avoid sync issues
+# Function to read CO2 concentration
+def read_co2_concentration():
+    try:
+        # Read CO2 concentration from register 0x0002
+        co2_concentration = instrument.read_register(0x0002, 0)  # No decimals, unsigned
+        print(f"CO2 Concentration: {co2_concentration} ppm")
 
+    except IOError:
+        print("Failed to read from instrument")
 
-
-# Read CO2 concentration (0x0002)
-co2_concentration = instrument.read_register(0x0002, 0)  # Reading as an integer (no decimals)
-print(f"CO2 Concentration: {co2_concentration} ppm")
-
-# Read Temperature (0x0000)
-temperature = instrument.read_register(0x0000, 1)  # Reading as a float (1 decimal place)
-print(f"Temperature: {temperature / 10.0} °C")     # The device returns temperature in tenths of a degree
-
-# Read Humidity (0x0001)
-humidity = instrument.read_register(0x0001, 1)    # Reading as a float (1 decimal place)
-print(f"Humidity: {humidity / 10.0} %RH")         # The device returns humidity in tenths of a percent
+# Continuously read and display CO2 concentration
+while True:
+    read_co2_concentration()
+    time.sleep(2)  # Delay between readings
--- python/src/Light Intensity/light.py
+++ python/src/Light Intensity/light.py
@@ -1,34 +1,28 @@
 '''
-  Author: Stephany Ayala-Cerna
+  Author: Stephany Ayala-Cerna, Vicente Arroyos
 '''
-
 import minimalmodbus
+import time
 
-mb_address = 1
+# Configure the instrument
+instrument = minimalmodbus.Instrument('/dev/ttyACM0', 1)  # Replace with your serial port and address
+instrument.serial.baudrate = 4800  # Default baudrate from the document
+instrument.serial.bytesize = 8
+instrument.serial.parity = minimalmodbus.serial.PARITY_NONE
+instrument.serial.stopbits = 1
+instrument.serial.timeout = 1  # seconds
 
-sensor = minimalmodbus.Instrument('/dev/ttyS0', mb_address)
+# Function to read light intensity
+def read_light_intensity():
+    try:
+        # Read light intensity from register 0x0006
+        light_intensity = instrument.read_register(0x0006, 0)  # No decimals, unsigned
+        print(f"Light Intensity: {light_intensity} Lux")
 
-sensor.serial.baudrate = 4800
-sensor.serial.bytesize = 8
-sensor.serial.parity = minimalmodbus.serial.PARITY_NONE
-sensor.serial.stopbits = 1
-sensor.serial.timeout = 0.5
-sensor.mode = minimalmodbus.MODE_RTU
+    except IOError:
+        print("Failed to read from instrument")
 
-# sensor.clear_buffers_before_each_transaction = True
-# sensor.close_port_after_each_call = True
-
-print("")
-print("Requesting Data From Sensor...")
-
-
-# Example of SINGLE Registers:
-# sensor.read_register(REGISTER ADDRESS, NUMBER OF DECIMALS, FUNCTION CODE, IS VALUE SIGNED OR UNSIGNED (TRUE OR FASLSE))
-
-# Example of MULTIPLE Registers
-# sensor.read_registers(REGISTER START ADDRESS, NUMBER OF REGISTERS TO READ, FUNCTION CODE)
-
-while (1):
-    data =  sensor.read_register(6, 0, 3, False)
-    # print(f"Raw Data is {data}")
-    print(f"Light Intensity: {data} Lux")
+# Continuously read and display light intensity
+while True:
+    read_light_intensity()
+    time.sleep(2)  # Delay between readings
